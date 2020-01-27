d0bae24669f573b4db9cd4da284a3ef2e98f875f
Petr Reshetin <preshetin@gmail.com>
2020-01-27 21:30:45 +0300

    Add prod config

--- src/config.js
+++ src/config.js
@@ -13,16 +13,16 @@ const dev = {
 };
 
 const prod = {
-  STRIPE_KEY: "",
+  STRIPE_KEY: "pk_test_LFPfy75whPn43YMRCOZ1WHH2",
   apiGateway: {
     REGION: "us-east-1",
-    URL: ""
+    URL: "https://gfawz339pf.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "",
-    APP_CLIENT_ID: "",
-    IDENTITY_POOL_ID: ""
+    USER_POOL_ID: "us-east-1_btaAsRGAW",
+    APP_CLIENT_ID: "5iei6gh9qmj8m5hiljthskki40",
+    IDENTITY_POOL_ID: "us-east-1:b504c32d-ef17-4db3-ade8-20f655e95f85"
   }
 };
 
